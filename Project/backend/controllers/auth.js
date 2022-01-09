38e55cd80eb2706227e6f1e70475ead60541e1e4
KhangNguyenPham <p.khangsnsd@gmail.com>
2022-01-09 14:01:18 +0700

    authentication

--- Project/backend/controllers/auth.js
+++ Project/backend/controllers/auth.js
@@ -1,10 +1,11 @@
 import {User} from '../models/User.js'
+import cryptoJs from 'crypto-js'
 
 export const register = async (req, res)=>{
     const newUser = new User({
         username : req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: cryptoJs.AES.encrypt(req.body.password, 'Secrect').toString(),
         role:req.body.role
     })
 
@@ -15,6 +16,21 @@ export const register = async (req, res)=>{
     }catch(err){
         res.status(500).json(err)
         console.log(err)
+    }
+}
+
+export const login = async (req, res)=>{
+    try{
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(401).json('Login Failed')
+        const hashedPassword = cryptoJs.AES.decrypt(user.password, 'Secrect')
+        const originalPassword = hashedPassword.toString(cryptoJs.enc.Utf8)
+        originalPassword !== req.body.password && 
+            res.status(401).json('Login Failed')
+        const {password, ...others} = user._doc
+        res.status(200).json(others)
+    }catch(err){
+        res.status(500).json(err)
     }
 }
 
